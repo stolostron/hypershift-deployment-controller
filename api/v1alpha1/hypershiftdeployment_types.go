23d203c99ad6afad9f6d51940619d716587a9ee8
Joshua Packer <jpacker@redhat.com>
2022-05-26 12:20:56 -0400

    Verify customized flow for AWS. (#96)
    1. Support changing up the AvailabilityPolicy
    2. Release.Image
    3. NodePool.Spec.Platform.AWS for the node's AMI information
    4. NodePool release image.
    5. Readme update with a reference to this YAML
    6. Fixed some bugs in this flow
    7. Fixed a poor condition message when spec.hostingCluster is missing
    8. Fixed a bug where we exited during a destroy early, orphaning resources
    
    Signed-off-by: Joshua Packer <jpacker@redhat.com>

--- api/v1alpha1/hypershiftdeployment_types.go
+++ api/v1alpha1/hypershiftdeployment_types.go
@@ -109,7 +109,6 @@ type HypershiftDeploymentSpec struct {
 	//If not specified, the controller will flag an error condition.
 	//The HostingCluster would be the management cluster of the hostedcluster and nodepool generated
 	//by the hypershiftDeployment
-	// +optional
 	HostingCluster string `json:"hostingCluster"`
 
 	// HostedCluster that will be applied to the ManagementCluster by ACM, if omitted, it will be generated
